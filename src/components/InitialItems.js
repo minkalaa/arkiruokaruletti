f00fd906a9ff7f32d02b1bdb9bea4492f4b02c4a
minka <minka.laasonen@gmail.com>
2021-09-02 13:09:47 +0300

    new item to the list

--- src/components/InitialItems.js
+++ src/components/InitialItems.js
@@ -25,6 +25,10 @@ const initialList = [
     {
         key: 'tonnikalapastavuoka',
         text: 'Tonnikalapastavuoka'
+    },
+    {
+        key: 'pyttipannu',
+        text: 'Pyttipannu'
     }
 ];
 
